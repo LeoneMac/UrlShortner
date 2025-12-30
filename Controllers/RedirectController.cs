68fcbe04bdc68958f9b1b5e7ebc26b2bc943ed91
Pedro <p20200177@gmail.com>
2025-12-30 19:10:17 -0300

    feat: add controller to redirect shortned url

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortner.Interfaces;
+
+namespace UrlShortner.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class RedirectController(IUrlService urlService) : ControllerBase
+    {
+        [HttpGet("{shortnedUrl}")]
+        public async Task<IActionResult> RedirectTo(string shortnedUrl)
+        {
+            string? originalUrl = await urlService.GetOriginalUrlByShortnedVersion(shortnedUrl);
+
+            if (originalUrl is null)
+            {
+                return NotFound($"Sorry, but the shortned url '{shortnedUrl}' doesn't exists.");
+            }
+
+            return Redirect(originalUrl);
+        }
+    }
+}
